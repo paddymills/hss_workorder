978e46fec5883f5963501d2098e84579200bb654
paddymills <pmiller1389@gmail.com>
2020-05-27 13:33:18 -0400

    added hss_core dependency

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         "xlwings>=0.19.4",
-        "pyodbc>=4.0.30"
-    ],
-    dependency_links=[
-        "git+https://github.com/paddymills/hss_core.git#egg=hss_core-0.1"
+        "pyodbc>=4.0.30",
+        "hss_core @ git+https://github.com/paddymills/hss_core#egg=hss_core-0.1"
     ]
 )
