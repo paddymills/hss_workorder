3039e88cc80612136d356c031ecdcd70b13b0593
paddymills <paddymills@pm.me>
2022-03-23 12:27:11 -0400

    ignorecase, workorder names, operations

--- src/lib/part.py
+++ src/lib/part.py
@@ -1,25 +1,34 @@
 
+from curses import setupterm
 from pyodbc import Row
 import re
 
-PLATE_COMM = re.compile(r"[PL|SHEET]")
-SHAPE_COMM = re.compile(r"[L|C|MC|W|WT]")
+PLATE_COMM = re.compile(r"PL|SHEET")
+SHAPE_COMM = re.compile(r"L|C|MC|W|WT")
 
 WO_REGEXES = {
-    "WEBS": re.compile(r"[A-Z]+[0-9]+[A-Z]*-(?:W[NF]?[0-9]+|X[A-Z])"),  # or clause added for backup bar (i.e. G1A-XA)
-    "FLGS": re.compile(r"[A-Z]+[0-9]+[A-Z]*-[TB][0-9]+"),
-    "G": re.compile(r"X[0-9]+[A-Z]+"),
-    "M": re.compile(r"M[0-9]+[A-Z]+"),
-    "S": re.compile(r"Z[0-9]+[A-Z]+"),
-    "PD": re.compile(r"PD[0-9]+-[A-Z]+"),
+    "WEBS": re.compile(r"[A-Z]+[0-9]+[A-Z]*-(?:W[NF]?[0-9]+|[A-Z]+)",   re.IGNORECASE),  # or clause added for backup bar (i.e. G1A-XA)
+    "FLGS": re.compile(r"[A-Z]+[0-9]+[A-Z]*-[TB][0-9]+",                re.IGNORECASE),
+    "G":    re.compile(r"X[0-9]+[A-Z]+",                                re.IGNORECASE),
+    "M":    re.compile(r"M[0-9]+[A-Z]+",                                re.IGNORECASE),
+    "S":    re.compile(r"Z[0-9]+[A-Z]+",                                re.IGNORECASE),
+    "PD":   re.compile(r"PD[0-9]+-[A-Z]+",                              re.IGNORECASE),
 }
 
 DEFAULT_PUNCH_THK = 0.625
-
+NUM_OPS = 3
 
 class Part:
 
-    def __init__(self, init_data=None, **kwargs):
+    def __init__(self, init_data=None, job=None, shipment=None, **kwargs):
+        self.job = job
+        self.shipment = shipment
+        self._workorder = None
+        self._ops = [None] * NUM_OPS
+
+        self.raw_mm = None
+        self.raw_mm_size = None
+
         for k, v in kwargs.items():
             setattr(self, k, v)
 
@@ -38,7 +47,7 @@ class Part:
         self.wid = row.Width
         self.len = row.Length
 
-        self.grade = MatlGrade(row.Specification, row.Grade, row.Test)
+        self.grade = MatlGrade(row.Specification, row.Grade, row.ImpactTest)
         self.remark = row.Remark
 
     @property
@@ -61,15 +70,45 @@ class Part:
         return not self.is_main
 
     @property
+    def workorder(self):
+        if self._workorder is None:
+
+            for name, regex in WO_REGEXES:
+                if regex.match(self.mark):
+                    self._workorder = name
+                    break
+            else:
+                self._workorder = "MISC"
+
+        return self._workorder
+
+    @property
     def wo_name(self):
-        for name, regex in WO_REGEXES:
-            if regex.match(self.mark):
-                return name
+        if self.job is None:
+            raise ValueError("No job specified")
+        if self.shipment is None:
+            raise ValueError("No shipment specified")
 
-        return "MISC"
+        return "-".join([self.job, self.shipment, self.workorder])
 
+    def set_workorder_name(self, workorder):
+        self.workorder = workorder
 
-    def infer_ops(self, min_punch=DEFAULT_PUNCH_THK):
+    @property
+    def sn_name(self):
+        if self.job is None:
+            raise ValueError("No job specified")
+
+        return "{}_{}".format(self.job, self.mark)
+
+    @property
+    def operations(self):
+        if self._ops is None:
+            self.infer_ops()
+
+        return self._ops
+
+    def infer_ops(self, min_punch=DEFAULT_PUNCH_THK, **kwargs):
         # Possible operations:
         #     - Punch
         #     - Drill
@@ -80,12 +119,12 @@ class Part:
         #     - Bevel
 
         # TODO: find if part has holes
-        has_holes = False
+        has_holes = kwargs.get("has_holes", False)
 
         find_remark = lambda x: re.search(x, self.remark, re.IGNORECASE)
-        has_end_mill = find_remark("b1e")
+        has_end_mill = kwargs.get("has_end_mill") or find_remark("b1e")
         bent = find_remark("bent")
-        can_punch = all([
+        can_punch = kwargs.get("can_punch") or all([
             find_remark("fill"),
             self.grade.test is None,
             self.thk <= min_punch,
@@ -93,7 +132,7 @@ class Part:
 
         ops = []
         # TODO: infer NX program
-        nx_program = False
+        nx_program = kwargs.get("nx_program", False)
 
         if nx_program:
             ops.append("NX")
@@ -112,18 +151,44 @@ class Part:
         if bent:
             ops.append("Press")
 
-        while len(ops) < 3:
+        while len(ops) < NUM_OPS:
             ops.append(None)
 
-        return ops
+        self._ops = ops
+
+    def set_ops(self, *ops):
+        assert len(ops) <= len(self._ops), "Too many operations, expected {}, got {}".format(len(self._ops), ops)
+        for i, op in enumerate(ops):
+            self._ops[i] = op
+
+    @property
+    def autoprocess_instruction(self):
+        if self.is_main:
+            return None
+
+        op_map = {
+            "Drill/End Mill": "Mill",
+            "Drill": "Drill",
+            "Punch": "Punch",
+        }
+
+        for op in self._ops:
+            if op in op_map:
+                return op_map[op]
+
+        return "Blank"
+    
+    def add_raw_matl(self, mm, size):
+        self.raw_mm = mm
+        self.raw_mm_size = size
 
 
 class MatlGrade:
 
     def __init__(self, spec, grade, test):
-        self.spec = spec
-        self.grade = grade
-        self.test = test
+        self.spec = spec or None
+        self.grade = grade or None
+        self.test = test or None
         self.zone = 2
 
         self.is_charpy_appl = True
@@ -146,8 +211,11 @@ class MatlGrade:
             self.test = "F"
 
     def __str__(self):
+        if self.spec is None or self.grade is None:
+            return ""
+
         if self.test:
-            test = self.test + self.zone
+            test = "{}{}".format(self.test, self.zone)
         else:
             test = ""
 
