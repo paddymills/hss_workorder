a594da43da80d4ca7afcc96397f4f8773c02c62a
Patrick Miller <paddymills@proton.me>
2024-08-30 10:00:04 -0400

    re-structure as single bin

--- cds/src/bin/cds.rs
+++
@@ -1,8 +0,0 @@
-
-use iced::{Application, Settings};
-
-use cds::CodeDeliverySystem;
-
-fn main() -> iced::Result {
-    CodeDeliverySystem::run(Settings::default())
-}
--- cds/src/lib.rs
+++
@@ -1,4 +0,0 @@
-
-mod frontend;
-
-pub use frontend::gui::CodeDeliverySystem;
